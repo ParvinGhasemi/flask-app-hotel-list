0fa8c83cb340b0d340564c508c408f93115f5eee
parvinghasemi <mghasemi.parvin@gmail.com>
2024-10-21 17:56:07 +0200

    tested url mapping successfully using decorator

--- src/demo.py
+++ src/demo.py
@@ -3,6 +3,11 @@ from flask import Flask
 app = Flask(__name__)
 print(__name__)
 
+@app.route('/index')
+@app.route('/')
+def index():
+    return <html><head><title>Hotels</title></head><body><h1> Hello! </h1></body></html>
+
 
 # so now let's detect whether or not the app is running as a standalone; if so run it on flask buit-in development server.
 # in order to do so, we're gonna use a decision construct:
